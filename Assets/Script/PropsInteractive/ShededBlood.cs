c8041a5aaa9e175287b45ff2f4e1bf50e541273e
jeantony <72211385+jeantony@users.noreply.github.com>
2022-06-19 23:21:40 +0200

    tache de sang qui s'étale

---
+++ Assets/Script/PropsInteractive/ShededBlood.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+
+public class ShededBlood : MonoBehaviour
+{
+    DecalProjector decal;
+
+    [SerializeField] Vector3 decalSize;
+
+    float x;
+    // Start is called before the first frame update
+    void Start()
+    {
+        decal = GetComponent<DecalProjector>();
+        decal.size = decalSize;
+        StartCoroutine("Sheded");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Sheded()
+    {
+        x += 0.1f;
+        decalSize = new Vector3(x, x, 5);
+        decal.size = decalSize;
+        yield return new WaitForSeconds(0.01f);
+
+        if(decal.size.x >=10)
+        {
+            decal.size = decalSize;
+        }
+        else
+        {
+            StartCoroutine("Sheded");
+        }
+    }
+}
